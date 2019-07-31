3a0987e919f80f361caf9215acc1e3b8f7b0ca03
abbas mirza <hasanabbas21@gmail.com>
2019-07-31 14:20:11 -0400

    circuit breaker

--- applications/mediabase/src/main/java/com/example/mediabase/Application.java
+++ applications/mediabase/src/main/java/com/example/mediabase/Application.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 @SpringBootApplication
 public class Application {
